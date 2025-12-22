f27cbc456af207d1b61726ddd25b442fcfd070c0
ELPADRI <elpadri91@gmail.com>
2025-12-22 18:33:44 +0700

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "elchat-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./firebase.js",
+  "./call.js",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (e)=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e)=>{
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
